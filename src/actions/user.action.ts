ee29088078f9ed464d28c1a4478685e222d4d7c4
ryansoe <ryansoe90686@gmail.com>
2025-07-26 21:52:48 -0700

    feat: like, comment, and delete posts

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -55,7 +55,7 @@ export async function getUserByClerkId(clerkId: string) {
 
 export async function getDbUserId() {
   const { userId:clerkId } = await auth();
-  if(!clerkId) throw new Error("Unauthorized");
+  if(!clerkId) return null;
 
   const user = await getUserByClerkId(clerkId);
 
@@ -67,6 +67,8 @@ export async function getDbUserId() {
 export async function getRandomUsers() {
   try {
     const userId = await getDbUserId();
+
+    if(!userId) return [];
 
     // get 2 random users exclude ourselves and users we already follow
     const randomUsers = await prisma.user.findMany({
@@ -108,6 +110,8 @@ export async function getRandomUsers() {
 export async function toggleFollow(targetUserId: string) {
   try {
     const userId = await getDbUserId();
+
+    if(!userId) return;
 
     if(userId === targetUserId) throw new Error("You cannot follow yourself");
 
