34b7d895d733e0d7aaf1dca35a48f6aab4ed6552
ryansoe <ryansoe90686@gmail.com>
2025-07-27 13:54:09 -0700

    feat: added image upload

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -9,6 +9,7 @@ import { Button } from "./ui/button";
 import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 import { createPost } from "@/actions/post.action";
 import toast from "react-hot-toast";
+import ImageUpload from "./ImageUpload";
 
 function CreatePost() {
   const {user} = useUser();
@@ -55,7 +56,18 @@ function CreatePost() {
           />
         </div>
 
-        {/* TODO: image upload */}
+        {(showImageUpload || imageUrl) && (
+            <div>
+                <ImageUpload
+                    onChange={(url) => {
+                        setImageUrl(url);
+                        setShowImageUpload(false);
+                    }}
+                    value={imageUrl}
+                    endpoint="postImage"
+                />
+            </div>
+        )}
 
         <div className="flex items-center justify-between border-t pt-4">
             <div className="flex space-x-2">
